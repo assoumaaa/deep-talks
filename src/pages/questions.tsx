3a398511607a009a2b0a2890154055ce60ced287
Omar Assouma <assouma@sabanciuniv.edu>
2023-07-28 21:30:21 +0300

    added title

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -48,31 +48,35 @@ const ShowQuestion = (questions: GetAllQuestions) => {
     );
 
   return (
-    <div className="flex w-11/12 flex-col items-center justify-center gap-6">
-      <span className="text-5xl italic text-black md:text-8xl">
-        {currentQuestion.content}
-      </span>
-      <div className="flex w-full justify-evenly ">
-        {currentQuestionIndex > 0 && (
+    <div className="flex h-full w-11/12 flex-col items-center justify-evenly ">
+      <span className="text-xl">General 🌎</span>
+      <div className="flex w-full flex-col gap-6">
+        <span className="text-center text-3xl italic text-black md:text-8xl">
+          {currentQuestion.content}
+        </span>
+        <div className="flex w-full justify-evenly ">
+          {currentQuestionIndex > 0 && (
+            <button
+              onClick={handlePrevQuestion}
+              className="group relative flex h-16 w-32 overflow-hidden rounded-lg bg-gradient-to-br from-primary to-secondary p-0.5 text-gray-900  focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
+            >
+              <span className="relative  flex h-full w-full items-center justify-center rounded-md bg-white p-2 text-2xl font-bold text-black transition-all duration-75 ease-in hover:text-white group-hover:bg-opacity-0">
+                Back
+              </span>
+            </button>
+          )}
+
           <button
-            onClick={handlePrevQuestion}
-            className="group relative flex h-16 w-32 overflow-hidden rounded-lg bg-gradient-to-br from-primary to-secondary p-0.5 text-gray-900  focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
+            onClick={handleNextQuestion}
+            className="group relative flex h-16 w-32 overflow-hidden rounded-lg bg-gradient-to-br from-primary to-secondary p-0.5 text-sm font-medium text-gray-900  focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
           >
             <span className="relative  flex h-full w-full items-center justify-center rounded-md bg-white p-2 text-2xl font-bold text-black transition-all duration-75 ease-in hover:text-white group-hover:bg-opacity-0">
-              Back
+              Next
             </span>
           </button>
-        )}
-
-        <button
-          onClick={handleNextQuestion}
-          className="group relative flex h-16 w-32 overflow-hidden rounded-lg bg-gradient-to-br from-primary to-secondary p-0.5 text-sm font-medium text-gray-900  focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
-        >
-          <span className="relative  flex h-full w-full items-center justify-center rounded-md bg-white p-2 text-2xl font-bold text-black transition-all duration-75 ease-in hover:text-white group-hover:bg-opacity-0">
-            Next
-          </span>
-        </button>
+        </div>
       </div>
+      <div></div>
     </div>
   );
 };
