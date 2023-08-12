d83775f5a14c0e2fdebaa79cfc5682cf104602e2
Omar Assouma <assouma@sabanciuniv.edu>
2023-08-12 18:42:19 +0300

    fiex bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <>
       <main>
-        <div className="flex h-screenWithNav w-screen flex-col items-center overflow-hidden md:flex-row">
-          <div className="flex  h-svh  w-full flex-col items-center justify-center space-y-12  p-12 ">
-            <div className="space-y-4 text-5xl leading-tight lg:text-7xl">
+        <div className="flex h-screenWithNav w-screen flex-col items-center  md:flex-row">
+          <div className="flex  h-svh  w-full flex-col items-center justify-center space-y-6  p-12 ">
+            <div className="space-y-4  text-4xl leading-tight lg:text-7xl">
               <h2>
                 Talk <span className="italic text-primary">Together,</span>
               </h2>
@@ -18,23 +18,26 @@ export default function Home() {
                 Thrive <span className="italic text-thirdly">Forever.</span>
               </h2>
             </div>
-            <span className="text-center text-xl font-light lg:text-2xl">
+            <span className="text-center text-base font-light lg:text-2xl">
               Unlock Meaningful Conversations, Bond Together Deeply.
             </span>
 
-            <div className="flex w-full justify-center ">
+            <div className="z-10  flex w-full justify-center ">
               <Link
                 href="/categories"
-                className="group relative flex h-16 w-1/2 overflow-hidden rounded-lg bg-gradient-to-br from-primary to-secondary p-0.5 text-sm font-medium text-gray-900  shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
+                className="group  relative flex h-14 w-1/3 overflow-hidden rounded-lg bg-gradient-to-br from-primary to-secondary p-0.5  shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
               >
-                <span className="relative  flex h-full w-full items-center justify-center rounded-md bg-white p-2 text-2xl font-bold text-black transition-all duration-75 ease-in hover:text-white group-hover:bg-opacity-0">
+                <span className="relative  flex h-full w-full items-center justify-center rounded-md bg-white p-2 text-lg font-bold text-black transition-all duration-75 ease-in hover:text-white group-hover:bg-opacity-0">
                   Get Started
                 </span>
               </Link>
             </div>
+            <div className=" md:hidden ">
+              <Image src={"/hero.png"} alt="hero" width={300} height={250} />
+            </div>
           </div>
-          <div className="flex h-full w-screen items-center justify-center md:flex">
-            <Image src={"/landing.png"} alt="hero" width={570} height={550} />
+          <div className=" hidden h-full w-screen items-center justify-center md:flex ">
+            <Image src={"/hero.png"} alt="hero" width={570} height={250} />
           </div>
         </div>
       </main>
